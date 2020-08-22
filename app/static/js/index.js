1eb0b9599839d43bf12bc5ab78b59efa5c2a03ed
binggr <52441587+binggr@users.noreply.github.com>
2020-08-23 01:37:08 +0800

    样式调整

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,41 +1,6 @@
-//点击城市添加active
-$("#city_id dd").click(function(){
-	$("#city_id dd").removeClass("active");
-	$(this).addClass("active");
-})
-
-//点击工作性质添加active
-$("#work_id dd").click(function(){
-	$("#work_id dd").removeClass("active");
-	$(this).addClass("active");
-})
-
-//点击投递属性添加active
-$("#property_id dd").click(function(){
-	$("#property_id dd").removeClass("active");
-	$(this).addClass("active");
-})
-
-//点击职能类型添加active
-$("#function_id dd").click(function(){
-	$("#function_id dd").removeClass("active");
-	$(this).addClass("active");
-})
-
-//点击企业性质添加active
-$("#enterprise_id dd").click(function(){
-	$("#enterprise_id dd").removeClass("active");
-	$(this).addClass("active");
-})
-
 //点击行业性质添加active
 $("#industry_id dd").click(function(){
 	$("#industry_id dd").removeClass("active");
 	$(this).addClass("active");
 })
 
-//点击行业性质添加active
-$("#sort_id span").click(function(){
-	$("#sort_id span").removeClass("active");
-	$(this).addClass("active");
-})
