f8dfef36b7445179e82c3869d28ddf03bb5789de
邱靖杰 <zmns@ZMNS.lan>
2020-08-26 01:38:55 +0800

    update routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
 from app import app
 from app.job import job
 from app.user import user
-
+from flask import url_for,redirect,request,render_template
+from werkzeug.exceptions import HTTPException
+import json
 
 app.register_blueprint(user, url_prefix='/user')
 app.register_blueprint(job, url_prefix='/job')
+
+@app.route('/')
+def home_page():
+    data = request.get_data()
+    data = json.load(data)
+    name = data.get('username')
+    if name is not None:
+        return redirect(url_for('/job.index'))
+    else:
+        return redirect(url_for('/user.login'))
+
+@app.errorhandler(HTTPException)
+def handle_bad_request(e):
+    index = "job/index"
+    return render_template('404.html',index=index), 404
